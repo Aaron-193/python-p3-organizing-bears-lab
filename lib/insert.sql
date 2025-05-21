f9255516c509497fe9cfde53c01dccbb9d093cff
anr.ron193@gmail.com <anr.ron193@gmail.com>
2025-05-21 16:29:57 +0300

    "updates organizing bears python and sql files"

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age sex, color, temperament,alive) VALUES 
+(1, 'Mr. Chocolate', 4, 'M', 'Brown', 'Calm', TRUE),
+(2, 'Rowdy', 7, 'M', 'Black', 'Aggressive', FALSE),
+(3, 'Tabitha', 5, 'F', 'White', 'Playful', TRUE),
+(4, 'Sergeant Brown', 8, 'M', 'Brown', 'Dominant', TRUE),
+(5, 'Melissa', 9, 'F', 'Black', 'Investigative', FALSE),
+(6, 'Grinch', 11, 'M', 'Black', 'Opportunistic', TRUE),
+(7, 'Wendy', 12, 'F', 'Brown', 'Protective', FALSE),
+(NULL, 6, 'F', 'Brown', 'Shy', TRUE);
